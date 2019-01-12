9300095fd08ca433919dafb2355995304bd9a9d5
Ethan Swan <eswan18@rocketmail.com>
2019-01-11 23:48:15 -0500

    fixed test data to be entirely serializable

--- api/tests/data/data.py
+++ api/tests/data/data.py
@@ -6,13 +6,13 @@ outdoor_run_doc = {'title': 'Outdoor Run',
                    'duration_seconds': 34,
                    'distance': 2.85,
                    'distance_unit': 'miles',
-                   'time': datetime(2018, 1, 1, 3, 30)}
+                   'time': '2019-01-11T17:00:00Z'}
 indoor_run_doc =  {'title': 'Treadmill Run',
                    'duration_minutes': 13,
                    'duration_seconds': 32,
                    'distance': 2,
                    'distance_unit': 'miles',
-                   'time': datetime(2020, 1, 2, 14, 12)}
+                   'time': '2020-01-02T14:12:00Z'}
 run_docs = [indoor_run_doc, indoor_run_doc, outdoor_run_doc, indoor_run_doc]
 # Example Lift Records
 push_lift_doc = {'title': 'Lift Push',
@@ -24,4 +24,4 @@ push_lift_doc = {'title': 'Lift Push',
                           {'exercise':   'Lat Raise', 'weight':  12, 'reps':  9, 'rest': 50},
                           {'exercise':   'Lat Raise', 'weight':  12, 'reps':  8, 'rest': 50},
                           {'exercise':   'Lat Raise', 'weight':  12, 'reps':  7, 'rest': 50}],
-                 'time': datetime(2015,  3, 4, 5, 46)}
+                 'time': '2015-03-04T05:46:00Z'}
