d497457cc92e0a6dde8f12e5f268ebde0e170727
Ethan Swan <eswan18@rocketmail.com>
2019-01-13 23:07:27 -0500

    added tests for GET user and workout with no id specified

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -17,8 +17,14 @@ def test_get_workout():
     resource = data['resource']
     assert resource['distance'] == 3.69
     assert resource['title'] == 'Outdoor Run'
+
 def test_get_all_workouts():
-    pass
+    response = requests.get(workout_url)
+    data = response.json()
+    resource = data['resource']
+    assert isinstance(resource, list)
+    assert len(resource) > 0
+    assert all([x.get('title') is not None for x in resource])
 
 def test_get_user():
     object_id = '5c3c03c7c62a6647e3204792'
@@ -27,3 +33,12 @@ def test_get_user():
     resource = data['resource']
     assert resource['first_name'] == 'Ethan'
     assert resource['last_name'] == 'Swan'
+
+def test_get_all_users():
+    response = requests.get(user_url)
+    data = response.json()
+    resource = data['resource']
+    assert isinstance(resource, list)
+    assert len(resource) > 0
+    assert all([x.get('first_name') is not None for x in resource])
+
