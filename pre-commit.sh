08ff918ca6da74b7cdc13f04c2a06026edfe26a7
Ethan Swan <eswan18@rocketmail.com>
2019-01-19 09:38:48 -0500

    modifications to commit hooks and tests

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,9 +3,13 @@ STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
 
 # Test prospective commit
+echo TESTING
 ./run_tests.sh
+exit_val=$?
 
 STASHES=$(git stash list)
 if [[ $STASHES == "$STASH_NAME" ]]; then
   git stash pop -q
 fi
+
+exit $exit_val
