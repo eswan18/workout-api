00d059c2bc889b45d4a3a75e2f97883781160ff4
Ethan Swan <eswan18@rocketmail.com>
2019-08-14 08:36:54 -0400

    Make incremental movement

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Canvas from './Canvas'
 
+// This is essentially just the ever-present frame around the changing canvas
+// within the app.
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Workout App</h1>
+        <Canvas/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ front-end/src/Canvas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Canvas () {
+  return (
+    <h1>Canvas</h1>
+  );
+}
+
+export default Canvas;
