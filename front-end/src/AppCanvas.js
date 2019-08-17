ef64423e14455de5b18272b3d32c54b0421f7c31
Ethan Swan <eswan18@rocketmail.com>
2019-08-16 22:42:26 -0400

    Add UserView -- doesn't work (CORS)

--- front-end/src/AppCanvas.js
+++ front-end/src/AppCanvas.js
@@ -1,11 +1,13 @@
 import './AppCanvas.css';
 import React from 'react';
+import UserView from './UserView.js'
 
 function AppCanvas () {
   return (
-    <div class="AppCanvas">
+    <div className="AppCanvas">
       <h1>App Canvas</h1>
       <p>App goes here...</p>
+      <UserView/>
     </div>
   );
 }
