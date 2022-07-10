9f197d4079f9a7cb481fe43b538d21c5be2d997b
Ethan Swan <eswan18@rocketmail.com>
2022-07-10 10:33:48 -0500

    Begin database module.

--- api/db/database.py
+++ api/db/database.py
@@ -1,7 +1,31 @@
-from pymongo import MongoClient
+import os
+from functools import cache
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 DB_URL = os.environ['DATABASE_URL']
 
-client = MongoClient(DB_URL)
-db = client.workout_api  # Fetch the "workout_api" database
+Base = declarative_base()
+
+
+@cache
+def get_engine() -> Engine:
+    return create_engine(DB_URL)
+
+
+def get_db():
+    engine = get_engine()
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+    )
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
