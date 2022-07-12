57ea46c15a45cf848913962f8a0b86b390e6398d
Ethan Swan <eswan18@rocketmail.com>
2022-07-11 22:22:05 -0500

    Try to fix heroku issues with bad URI.

--- scripts/run_seeds.sh
+++ scripts/run_seeds.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Heroku uses an old URI style that we have to fix.
+old="postgres://"
+new="postgresql://"
+db_url="${DATABASE_URL/"$old"/"$new"}"
+
 here=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-psql $DATABASE_URL -f $here/seeds/*
+psql $db_url -f $here/seeds/*
