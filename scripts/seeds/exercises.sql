1d44353cbde4012e9a10bb25549820395f082cbd
Ethan Swan <eswan18@rocketmail.com>
2022-07-12 18:48:45 -0500

    Draft seeds.

--- scripts/seeds/exercises.sql
+++ scripts/seeds/exercises.sql
@@ -1,5 +1,6 @@
 INSERT INTO exercises (id, name, number_of_weights, notes) VALUES
 ('8278d21c-1ffa-4dbf-b5b0-070cdb7824cd', 'Bicep Curls', 2, NULL),
+('5d4d6ef0-5e14-4506-8525-87d3b1903568', 'Seated rows', 1, '(machine)'),
 ('1624bfc4-e191-49c7-8173-118058570ac0', 'Barbell Squats', 1, 'Just the normal ones, not front squats'),
 ('bef96a30-3dd5-4c94-abbb-e822301e7083', 'Planks', 0, 'This is one where we keep track of seconds not reps')
 ;
---
+++ scripts/seeds/workout_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO workout_types (id, name, notes, parent_workout_type_id) VALUES
+('3d531b3b-907f-488b-b2b6-ad3e541d08fd', 'Upper Body', 'Upper body exercises', NULL),
+('72c29956-f71a-4e6a-b8e2-d6ea2008ed35', 'Pull Day', 'Biceps and upper back exercises', '3d531b3b-907f-488b-b2b6-ad3e541d08fd'),
+('a4f69cd3-12ba-42ec-8ffc-b03a167f489e', 'Push Day', 'Chest, shoulders, and triceps exercises', '3d531b3b-907f-488b-b2b6-ad3e541d08fd')
+;
