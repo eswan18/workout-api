293894f664a47de09a93701ce83e093e7fe8bc03
Ethan Swan <eswan18@rocketmail.com>
2022-07-11 19:41:48 -0500

    Make models importable from api.db.models.

---
+++ api/db/models/__init__.py
@@ -0,0 +1,7 @@
+from .exercise import Exercise
+from .set import Set
+from .user import User
+from .workout import Workout
+from .workout_type import WorkoutType
+
+__all__ = ['Exercise', 'Set', 'User', 'Workout', 'WorkoutType']
