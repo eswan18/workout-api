548ab22e1019a433a579bffff8f534e13362fda9
Ethan Swan <eswan18@rocketmail.com>
2022-07-11 22:05:29 -0500

    Update workout and workout_type models for ORM.

--- api/v1/models/workout.py
+++ api/v1/models/workout.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from datetime import datetime
 
 from pydantic import BaseModel
@@ -8,18 +9,24 @@ class Workout(BaseModel):
     end_time: datetime | None
     status: str
     notes: str | None
-    workout_type: str | None
+    workout_type_id: UUID | None
 
 
 class WorkoutInDB(Workout):
-    id: str
+    id: UUID
+
+    class Config:
+        orm_mode = True
 
 
 class WorkoutType(BaseModel):
     name: str
-    notes: str
-    parent_workout_type_id: str | None
+    notes: str | None
+    parent_workout_type_id: UUID | None
 
 
 class WorkoutTypeInDB(WorkoutType):
-    id: str
+    id: UUID
+
+    class Config:
+        orm_mode = True
