59f2f45e79993b055ee5d90bc37a03f086f575d7
Ethan Swan <eswan18@rocketmail.com>
2022-07-13 19:07:05 -0500

    Simplify v1/exercises route.

--- api/v1/exercises.py
+++ api/v1/exercises.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter, Depends
-
 from sqlalchemy.orm import Session
 
 from .models.exercise import Exercise, ExerciseInDB
 from ..db import models as db_models
 from ..db import get_db
 
-router = APIRouter(
-    prefix="/exercises",
-    dependencies=[Depends(get_db)],
-)
+router = APIRouter(prefix="/exercises")
 
 
 @router.get("/", response_model=list[Exercise])
