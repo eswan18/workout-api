f464422938c8ea8a1805d5515be1d04a224b9ec4
Ethan Swan <eswan18@rocketmail.com>
2022-07-16 19:34:15 -0500

    Lock down exercises by user.

--- api/v1/exercises.py
+++ api/v1/exercises.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from .models.exercise import Exercise, ExerciseInDB
+from .auth import get_current_user
 from ..db import models as db_models
 from ..db import get_db
 
@@ -9,7 +10,10 @@ router = APIRouter(prefix="/exercises")
 
 
 @router.get("/", response_model=list[Exercise])
-async def exercises(db: Session = Depends(get_db)):
+async def exercises(
+    db: Session = Depends(get_db),
+    current_user: db_models.User = Depends(get_current_user),
+) -> list[Exercise]:
     result = db.query(db_models.Exercise).all()
     records = [ExerciseInDB.from_orm(row) for row in result]
     return records
