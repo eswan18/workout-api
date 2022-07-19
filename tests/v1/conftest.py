27a5f9673e55c958627a7e443ffe3edde42af455
Ethan Swan <eswan18@rocketmail.com>
2022-07-18 22:58:35 -0500

    Get dep injection for current user working.

--- tests/v1/conftest.py
+++ tests/v1/conftest.py
@@ -1,8 +1,29 @@
+from uuid import UUID
+from typing import Iterator
+
 from fastapi.testclient import TestClient
 import pytest
 
-from app.v1 import app
+from app.v1 import app, auth
+from app.db import models as db_models
 
-@pytest.fixture
+
+@pytest.fixture(scope="session")
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture(scope="function")
+def fake_current_user(client) -> Iterator[db_models.User]:
+    user = db_models.User(
+        id=UUID("117e87dd-9f1a-4f20-a4c1-4fa646077370"),  # type: ignore
+        email="elend@elendel.gov",
+        pw_hash="16161616",
+    )
+
+    async def get_fake_user(token=None, db=None) -> db_models.User:
+        return user
+
+    client.app.dependency_overrides[auth.get_current_user] = get_fake_user
+    yield user
+    del client.app.dependency_overrides[auth.get_current_user]
--- tests/v1/endpoints/test_users.py
+++ tests/v1/endpoints/test_users.py
@@ -1,3 +1,20 @@
+from app.v1.models.user import UserOut
+
+
 def test_get_me_fails_without_auth(client):
+    """
+    /users/me fails without valid credentials.
+    """
     response = client.get("/users/me")
     assert response.status_code == 401
+
+
+def test_get_me_returns_current_user(client, fake_current_user):
+    """
+    /users/me returns the corrent user model, with the expected fields.
+    """
+    response = client.get("/users/me")
+    assert response.status_code == 200
+    expected_payload = UserOut.from_orm(fake_current_user)
+    actual_payload = UserOut.parse_obj(response.json())
+    assert actual_payload == expected_payload
