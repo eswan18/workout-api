e2bb4559e97939e31a633a0b34aea0c6f05fa3fe
Ethan Swan <eswan18@rocketmail.com>
2022-08-04 17:09:24 -0500

    Add CORS localhost exclusions.

--- app/v1/main.py
+++ app/v1/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .endpoints import routers
 
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
 v1_app = FastAPI()
+v1_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @v1_app.get("/")
