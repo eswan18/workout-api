d9d9357ddc65564e42c033ccbdc530ec7c49057e
Ethan Swan <ethanpswan@gmail.com>
2023-02-11 21:58:02 -0600

    Add owner_user_id to exercises table.

--- app/db/models/exercise.py
+++ app/db/models/exercise.py
@@ -1,9 +1,11 @@
 import uuid
 
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, ForeignKey, Integer, Text
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship, Mapped
 
 from ..database import Base
+from .user import User
 
 
 class Exercise(Base):
@@ -13,3 +15,8 @@ class Exercise(Base):
     name: str = Column(Text, nullable=False)
     number_of_weights: int = Column(Integer, default=1, nullable=False)
     notes: str | None = Column(Text)
+
+    owner_user_id: UUID | None = Column(
+        UUID(as_uuid=True), ForeignKey("users.id"), nullable=True
+    )
+    user: Mapped[User] = relationship(User, backref="owned_exercises")
