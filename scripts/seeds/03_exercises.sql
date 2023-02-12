10df7a843a90b34b8802fdf1cf3d7cb969874ccc
Ethan Swan <ethanpswan@gmail.com>
2023-02-11 22:11:24 -0600

    Add exercise with owner_user_id to seeds.

--- scripts/seeds/03_exercises.sql
+++ scripts/seeds/03_exercises.sql
@@ -1,6 +1,7 @@
-INSERT INTO exercises (id, name, number_of_weights, notes) VALUES
-('8278d21c-1ffa-4dbf-b5b0-070cdb7824cd', 'Bicep Curls', 2, NULL),
-('5d4d6ef0-5e14-4506-8525-87d3b1903568', 'Seated rows', 1, '(machine)'),
-('1624bfc4-e191-49c7-8173-118058570ac0', 'Barbell Squats', 1, 'Just the normal ones, not front squats'),
-('bef96a30-3dd5-4c94-abbb-e822301e7083', 'Planks', 0, 'This is one where we keep track of seconds not reps')
+INSERT INTO exercises (id, name, number_of_weights, notes, owner_user_id) VALUES
+('8278d21c-1ffa-4dbf-b5b0-070cdb7824cd', 'Bicep Curls', 2, NULL, NULL),
+('5d4d6ef0-5e14-4506-8525-87d3b1903568', 'Seated rows', 1, '(machine)', NULL),
+('1624bfc4-e191-49c7-8173-118058570ac0', 'Barbell Squats', 1, 'Just the normal ones, not front squats', NULL),
+('bef96a30-3dd5-4c94-abbb-e822301e7083', 'Planks', 0, 'This is one where we keep track of seconds not reps', NULL),
+('feb588b5-e86b-4269-8121-475682df2830', 'Dummys Deadlift', 0, 'Only dummy account knows this secret exercise', 'ddf909e8-1ef9-4cdc-8476-732137a352d8')
 ;
