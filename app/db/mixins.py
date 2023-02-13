432368704a189cc29045fc75eb7e80f8a7ab87df
Ethan Swan <ethanpswan@gmail.com>
2023-02-12 20:13:16 -0600

    Update ModificationTimesMixin for sqlalchemy 2.0.

--- app/db/mixins.py
+++ app/db/mixins.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column
+from datetime import datetime
+
 from sqlalchemy.sql import func
 from sqlalchemy.types import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class ModificationTimesMixin:
@@ -8,13 +10,12 @@ class ModificationTimesMixin:
     Mixin for standard created_at, updated_at, deleted_at columns.
     """
 
-    created_at = Column(
-        DateTime(timezone=True), nullable=False, server_default=func.now()
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
     )
-    updated_at = Column(
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        nullable=False,
         server_default=func.now(),
         onupdate=func.now(),
     )
-    deleted_at = Column(DateTime(timezone=True))
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
