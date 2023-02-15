1ed7e96b1f024702c7ac21531fc96db5848ed072
Ethan Swan <ethanpswan@gmail.com>
2023-02-14 21:58:03 -0600

    Add filtering to exercise types.

--- app/db/models/exercise_type.py
+++ app/db/models/exercise_type.py
@@ -1,6 +1,9 @@
 import uuid
+from typing import Self
 
-from sqlalchemy import ForeignKey, Integer, Text, UUID
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.types import Integer, Text, UUID
+from sqlalchemy.sql import Select
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from app.db.database import Base
@@ -23,3 +26,33 @@ class ExerciseType(Base, ModificationTimesMixin):
     )
     # Relationships
     owner: Mapped[User] = relationship(User, backref="owned_exercises")
+
+    @classmethod
+    def apply_params(
+        cls,
+        query: Select[tuple[Self]],
+        *,
+        id: uuid.UUID | None = None,
+        name: str | None = None,
+        owner_user_id: uuid.UUID | None = None,
+    ) -> Select[tuple[Self]]:
+        if id is not None:
+            query = query.filter(cls.id == id)
+        if name is not None:
+            query = query.filter(cls.name == name)
+        if owner_user_id is not None:
+            query = query.filter(cls.owner_user_id == owner_user_id)
+        return query
+
+    @classmethod
+    def apply_read_permissions(
+        cls,
+        query: Select[tuple[Self]],
+        user: User,
+    ) -> Select[tuple[Self]]:
+        """
+        Limit a query down to only the resources a user has access to read.
+        """
+        # Users can access workout types that they own or that are public, denoted as a
+        # null value in owner_user_id.
+        return query.where((cls.owner == user) | (cls.owner == None))  # noqa
--- app/v1/endpoints/exercise_types.py
+++ app/v1/endpoints/exercise_types.py
@@ -6,8 +6,7 @@ from sqlalchemy.orm import Session
 
 from ..models.exercise_type import ExerciseTypeInDB, ExerciseTypeIn
 from ..auth import get_current_user
-from ...db import models as db_models
-from ...db import get_db
+from app import db
 
 router = APIRouter(prefix="/exercise_types")
 
@@ -17,48 +16,35 @@ def exercise_types(
     id: UUID | None = None,
     name: str | None = None,
     owner_user_id: UUID | None = None,
-    db: Session = Depends(get_db),
-    current_user: db_models.User = Depends(get_current_user),
+    session: Session = Depends(db.get_db),
+    current_user: db.User = Depends(get_current_user),
 ) -> list[ExerciseTypeInDB]:
     """
     Fetch all accessible exercise types.
     """
-    query = select(db_models.ExerciseType)
-
-    # Filters
-    eq_filters = {"id": id, "name": name, "owner_user_id": owner_user_id}
-    # Ignore any that weren't provided.
-    eq_filters = {
-        key: value for (key, value) in eq_filters.items() if value is not None
-    }
-    query = query.filter_by(**eq_filters)
-
-    # Permisions
-    # Only return records that are owned by this user or "public", meaning their owner
-    # field is null.
-    query = query.where(
-        (db_models.ExerciseType.owner == None)  # noqa
-        | (db_models.ExerciseType.owner == current_user)
+    query = select(db.ExerciseType)
+    query = db.ExerciseType.apply_params(
+        query, id=id, name=name, owner_user_id=owner_user_id
     )
+    query = db.ExerciseType.apply_read_permissions(query, current_user)
 
-    result = db.scalars(query)
+    result = session.scalars(query)
     records = [ExerciseTypeInDB.from_orm(ex_tp) for ex_tp in result]
-
     return records
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=ExerciseTypeInDB)
 def create_exercise(
     exercise_type: ExerciseTypeIn,
-    db: Session = Depends(get_db),
-    current_user: db_models.User = Depends(get_current_user),
-) -> db_models.ExerciseType:
+    session: Session = Depends(db.get_db),
+    current_user: db.User = Depends(get_current_user),
+) -> db.ExerciseType:
     """
     Create a new exercise type.
     """
-    exercise_type_record = db_models.ExerciseType(**exercise_type.dict())
+    exercise_type_record = db.ExerciseType(**exercise_type.dict())
     exercise_type_record.owner_user_id = current_user.id
-    db.add(exercise_type_record)
-    db.commit()
-    db.refresh(exercise_type_record)
+    session.add(exercise_type_record)
+    session.commit()
+    session.refresh(exercise_type_record)
     return exercise_type_record
