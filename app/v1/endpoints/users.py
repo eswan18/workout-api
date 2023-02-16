dc589b3249b67eacafafc8053fb6c65da9a04685
Ethan Swan <ethanpswan@gmail.com>
2023-02-15 22:54:28 -0600

    Clean up users endpoint file.

--- app/v1/endpoints/users.py
+++ app/v1/endpoints/users.py
@@ -2,7 +2,7 @@ import os
 
 from fastapi import APIRouter, Depends, status, HTTPException, Body
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from app.v1.models.user import UserIn, UserOut
 from app.v1.auth import get_current_user
@@ -18,12 +18,11 @@ router = APIRouter(prefix="/users")
 def create_user(
     user: UserIn,
     secret: str = Body(),
-    session: Session = Depends(db.get_db),
+    session_factory: sessionmaker[Session] = Depends(db.get_session_factory),
 ) -> db.User:
     """
     Create a new user. Requires a secret string, for now.
     """
-    # The worst auth ever, but works until I add a Captcha or something.
     if secret != USER_CREATION_SECRET:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -34,24 +33,24 @@ def create_user(
     password = user.password
     hashed_pw = hash_pw(email, password)
 
-    user_record = db.User(email=email, pw_hash=hashed_pw)
-    try:
-        session.add(user_record)
-        session.commit()
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="an account with that email address is already in use",
-        )
-    session.refresh(user_record)
-    return user_record
+    record = db.User(email=email, pw_hash=hashed_pw)
+    with session_factory(expire_on_commit=False) as session:
+        session.add(record)
+        try:
+            session.commit()
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="an account with that email address is already in use",
+            )
+    return record
 
 
 @router.get("/me", response_model=UserOut)
 def get_me(
     current_user: db.User = Depends(get_current_user),
-) -> UserOut:
+) -> db.User:
     """
     Fetch information about your own user.
     """
-    return UserOut(id=current_user.id, email=current_user.email)
+    return current_user
