a12c2f2f69201ed34d1ab899ce0a547225deae7b
Ethan Swan <ethanpswan@gmail.com>
2023-02-19 20:21:56 -0600

    Add secondary user in test fixtures.

--- tests/v1/conftest.py
+++ tests/v1/conftest.py
@@ -52,7 +52,36 @@ def primary_test_user(session_factory) -> Iterator[UserWithAuth]:
     # Setup: create a unique, fake user.
     user_string = "".join(choices(ascii_letters, k=15))
     password = "".join(choices(ascii_letters, k=15))
-    unique_email = f"testuser-{user_string}@example.com"
+    unique_email = f"testuser1-{user_string}@example.com"
+    pw_hash = hash_pw(unique_email, password)
+    user = db.User(
+        id=uuid4(),
+        email=unique_email,
+        pw_hash=pw_hash,
+    )
+    # Insert it in the db.
+    with session_factory(expire_on_commit=False) as session:
+        session.add(user)
+        session.commit()
+
+    jwt = generate_jwt(user.email)
+    auth_header = {"Authorization": f"Bearer {jwt['access_token']}"}
+
+    yield UserWithAuth(user, auth_header)
+
+    # Teardown: delete from db.
+    recursive_hard_delete(user.id, session_factory)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def secondary_test_user(session_factory) -> Iterator[UserWithAuth]:
+    """
+    Create a temporary test user and corresponding auth for the duration of testing.
+    """
+    # Setup: create a unique, fake user.
+    user_string = "".join(choices(ascii_letters, k=15))
+    password = "".join(choices(ascii_letters, k=15))
+    unique_email = f"testuser2-{user_string}@example.com"
     pw_hash = hash_pw(unique_email, password)
     user = db.User(
         id=uuid4(),
