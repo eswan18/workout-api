535a85919e64a47e53e7b972851f1b5f40d84605
Ethan Swan <ethanpswan@gmail.com>
2023-02-19 22:08:31 -0600

    Fix some issues in workout types tests.

--- tests/v1/endpoints/test_workout_types.py
+++ tests/v1/endpoints/test_workout_types.py
@@ -1,10 +1,14 @@
+from uuid import UUID
+from typing import Iterable
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.sql import select, delete
+from sqlalchemy.sql.functions import count
 from sqlalchemy.orm import sessionmaker, Session
 
-from app.db.models.user import User, UserWithAuth
+from app.db.models.user import UserWithAuth
 from app.db import WorkoutType
-
 
 ROUTE = "/workout_types/"
 
@@ -18,24 +22,32 @@ def postable_payload():
 
 
 @pytest.fixture(scope="function")
-def wts_in_db(
+def primary_user_workout_types(
     session_factory: sessionmaker[Session], primary_test_user: UserWithAuth
-) -> User:
+) -> Iterable[tuple[WorkoutType, WorkoutType]]:
+    """Add workout types to the db owned by the primary user."""
     user_id = primary_test_user.user.id
     with session_factory(expire_on_commit=False) as session:
-        rows = [
+        rows = (
             WorkoutType(
                 name="a new workout 1",
                 owner_user_id=user_id,
             ),
             WorkoutType(
                 name="a new workout 2",
-                owner_user_id=None,
+                owner_user_id=user_id,
             ),
-        ]
+        )
         session.add_all(rows)
         session.commit()
-    return rows
+
+    yield rows
+
+    with session_factory() as session:
+        session.execute(
+            delete(WorkoutType).where(WorkoutType.id.in_(row.id for row in rows))
+        )
+        session.commit()
 
 
 #########
@@ -60,6 +72,7 @@ def test_authenticated_user_can_create_workout_types(
     client: TestClient,
     primary_test_user: UserWithAuth,
     postable_payload: dict[str, str],
+    session_factory: sessionmaker[Session],
 ):
     response = client.post(ROUTE, json=postable_payload, headers=primary_test_user.auth)
     assert response.status_code == 201
@@ -69,6 +82,13 @@ def test_authenticated_user_can_create_workout_types(
     resource = payload[0]
     assert "id" in resource
 
+    # Clean up
+    with session_factory() as session:
+        session.execute(
+            delete(WorkoutType).where(WorkoutType.id == UUID(resource["id"]))
+        )
+        session.commit()
+
 
 #########
 # Reads
@@ -77,19 +97,54 @@ def test_authenticated_user_can_create_workout_types(
 
 def test_unauthenticated_user_cant_read(
     client: TestClient,
-    postable_payload: dict[str, str],
     primary_test_user: UserWithAuth,
+    primary_user_workout_types: tuple[WorkoutType, WorkoutType],
 ):
     # Try with no creds.
-    response = client.post(ROUTE, json=postable_payload)
+    response = client.get(ROUTE)
     # Make sure we get a 401 and no data comes back.
     assert response.status_code == 401
     assert set(response.json().keys()) == {"detail"}
 
     # Try with bad creds.
-    bad_user_auth = primary_test_user.auth
+    bad_user_auth = primary_test_user.auth.copy()
     bad_user_auth.update(Authorization="Bearer 123abc")
-    response = client.post(ROUTE, json=postable_payload, headers=bad_user_auth)
+    response = client.get(ROUTE, headers=bad_user_auth)
     # Make sure we get a 401 and no data comes back.
     assert response.status_code == 401
     assert set(response.json().keys()) == {"detail"}
+
+
+def test_one_user_cant_read_anothers_workout_types(
+    client: TestClient,
+    secondary_test_user: UserWithAuth,
+    primary_user_workout_types: tuple[WorkoutType, WorkoutType],
+):
+    response = client.get(ROUTE, headers=secondary_test_user.auth)
+    assert response.status_code == 200
+    # Make sure we only get public workout types.
+    workout_types = response.json()
+    for workout_type in workout_types:
+        assert workout_type["owner_user_id"] == None
+
+
+def test_user_can_read_own_and_public_workout_types(
+    client: TestClient,
+    primary_test_user: UserWithAuth,
+    primary_user_workout_types: tuple[WorkoutType, WorkoutType],
+    session_factory: sessionmaker[Session],
+):
+    # How many public workouts should there be?
+    with session_factory() as session:
+        n_public_workout_types = session.scalar(
+            select(count(WorkoutType.id)).where(WorkoutType.owner_user_id == None)
+        )
+    # How many private workouts should there be?
+    n_private_workout_types = len(primary_user_workout_types)
+
+    response = client.get(ROUTE, headers=primary_test_user.auth)
+    assert response.status_code == 200
+    workout_types = response.json()
+    assert len(workout_types) == (n_public_workout_types + n_private_workout_types)
+    for workout_type in workout_types:
+        assert workout_type["owner_user_id"] in (None, str(primary_test_user.user.id))
