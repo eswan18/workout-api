0db4774a547a01a126a653374e21b221dc4e25ce
Ethan Swan <ethanpswan@gmail.com>
2023-02-19 20:21:25 -0600

    Add test of unauth reading of workout types.

--- tests/v1/endpoints/test_workout_types.py
+++ tests/v1/endpoints/test_workout_types.py
@@ -1,7 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import sessionmaker, Session
 
-from app.db.models.user import UserWithAuth
+from app.db.models.user import User, UserWithAuth
+from app.db import WorkoutType
 
 
 ROUTE = "/workout_types/"
@@ -13,6 +15,32 @@ def postable_payload():
         "name": "Leg dayyyyy",
         "notes": "this is the one you usually skip",
     }
+
+
+@pytest.fixture(scope="function")
+def wts_in_db(
+    session_factory: sessionmaker[Session], primary_test_user: UserWithAuth
+) -> User:
+    user_id = primary_test_user.user.id
+    with session_factory(expire_on_commit=False) as session:
+        rows = [
+            WorkoutType(
+                name="a new workout 1",
+                owner_user_id=user_id,
+            ),
+            WorkoutType(
+                name="a new workout 2",
+                owner_user_id=None,
+            ),
+        ]
+        session.add_all(rows)
+        session.commit()
+    return rows
+
+
+#########
+# Creates
+#########
 
 
 def test_unauthenticated_user_cant_create_workout_types(
@@ -40,3 +68,28 @@ def test_authenticated_user_can_create_workout_types(
     assert len(payload) == 1
     resource = payload[0]
     assert "id" in resource
+
+
+#########
+# Reads
+#########
+
+
+def test_unauthenticated_user_cant_read(
+    client: TestClient,
+    postable_payload: dict[str, str],
+    primary_test_user: UserWithAuth,
+):
+    # Try with no creds.
+    response = client.post(ROUTE, json=postable_payload)
+    # Make sure we get a 401 and no data comes back.
+    assert response.status_code == 401
+    assert set(response.json().keys()) == {"detail"}
+
+    # Try with bad creds.
+    bad_user_auth = primary_test_user.auth
+    bad_user_auth.update(Authorization="Bearer 123abc")
+    response = client.post(ROUTE, json=postable_payload, headers=bad_user_auth)
+    # Make sure we get a 401 and no data comes back.
+    assert response.status_code == 401
+    assert set(response.json().keys()) == {"detail"}
