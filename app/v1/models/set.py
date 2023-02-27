94fb31865aace0c4e46ecdb304452a650dff5bc5
Ethan Swan <ethanpswan@gmail.com>
2023-02-27 07:57:31 -0600

    Add ORM methods on SetIn.

--- app/v1/models/set.py
+++ app/v1/models/set.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 from pydantic import BaseModel
 
+from app.db.models import Set
+
 
 class SetIn(BaseModel):
     start_time: datetime | None
@@ -14,6 +16,35 @@ class SetIn(BaseModel):
     # Relations
     exercise_type_id: UUID
     workout_id: UUID
+
+    def to_orm_model(self, user_id: UUID | None) -> Set:
+        """
+        Convert to a sqlalchemy model object.
+        """
+        return Set(
+            start_time=self.start_time,
+            weight=self.weight,
+            weight_unit=self.weight_unit,
+            reps=self.reps,
+            seconds=self.seconds,
+            notes=self.notes,
+            exercise_type_id=self.exercise_type_id,
+            workout_id=self.workout_id,
+            user_id=user_id,
+        )
+
+    def update_orm_model(self, model: Set) -> None:
+        """
+        Update a sqlalchemy object in-place with these values.
+        """
+        model.start_time = self.start_time
+        model.weight = self.weight
+        model.weight_unit = self.weight_unit
+        model.reps = self.reps
+        model.seconds = self.seconds
+        model.notes = self.notes
+        model.exercise_type_id = self.exercise_type_id
+        model.workout_id = self.workout_id
 
 
 class SetInDB(SetIn):
