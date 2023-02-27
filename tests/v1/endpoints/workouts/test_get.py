0b2aefe008535ba8dd88f5f3fd53b73a25e4e19d
Ethan Swan <ethanpswan@gmail.com>
2023-02-27 06:51:18 -0600

    Add some tests for workout endpoints.

---
+++ tests/v1/endpoints/workouts/test_get.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+
+from app.db.models.user import UserWithAuth
+from app.db import Workout
+
+
+ROUTE = "/workouts/"
+
+
+def test_unauthenticated_user_cant_read(
+    client: TestClient,
+    primary_test_user: UserWithAuth,
+    primary_user_workouts: tuple[Workout, ...],
+):
+    # Try with no creds.
+    response = client.get(ROUTE)
+    # Make sure we get a 401 and no data comes back.
+    assert response.status_code == 401
+    assert set(response.json().keys()) == {"detail"}
+
+    # Try with bad creds.
+    bad_user_auth = primary_test_user.auth.copy()
+    bad_user_auth.update(Authorization="Bearer 123abc")
+    response = client.get(ROUTE, headers=bad_user_auth)
+    # Make sure we get a 401 and no data comes back.
+    assert response.status_code == 401
+
+
+def test_one_user_cant_read_anothers_workout_types(
+    client: TestClient,
+    secondary_test_user: UserWithAuth,
+    primary_user_workouts: tuple[Workout, ...],
+):
+    response = client.get(ROUTE, headers=secondary_test_user.auth)
+    assert response.status_code == 200
+    # This user owns zero workouts.
+    workouts = response.json()
+    assert len(workouts) == 0
+
+
+def test_user_can_read_own_workouts(
+    client: TestClient,
+    primary_test_user: UserWithAuth,
+    primary_user_workouts: tuple[Workout, ...],
+    primary_user_soft_deleted_workout: Workout,
+):
+    """
+    Users can read their own workouts but not soft deleted ones.
+    """
+    response = client.get(ROUTE, headers=primary_test_user.auth)
+    assert response.status_code == 200
+    workouts = response.json()
+    assert len(workouts) == len(primary_user_workouts)
+    for workout in workouts:
+        assert workout["user_id"] == str(primary_test_user.user.id)
