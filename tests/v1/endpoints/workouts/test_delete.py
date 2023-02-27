05e4fe16733193bde127880a303ec13c368236c5
Ethan Swan <ethanpswan@gmail.com>
2023-02-27 07:19:58 -0600

    Finish DELETE tests for workouts/ endpoint.

--- tests/v1/endpoints/workouts/test_delete.py
+++ tests/v1/endpoints/workouts/test_delete.py
@@ -1,31 +1,29 @@
 from datetime import datetime, timezone
-
 from fastapi.testclient import TestClient
-from sqlalchemy.sql import select
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.sql import select
 
 from app.db.models.user import UserWithAuth
-from app.db import WorkoutType
+from app.db import Workout
 
-
-ROUTE = "/workout_types/"
+ROUTE = "/workouts/"
 
 
 def test_authentication_required_for_delete(
     client: TestClient,
     secondary_test_user: UserWithAuth,
-    primary_user_workout_types: tuple[WorkoutType, ...],
+    primary_user_workouts: tuple[Workout, ...],
     session_factory: sessionmaker[Session],
 ):
-    wt_to_delete = primary_user_workout_types[0]
-    params = {"id": wt_to_delete.id}
+    wkt_to_delete = primary_user_workouts[0]
+    params = {"id": wkt_to_delete.id}
 
     # No auth should get a 401.
     response = client.delete(ROUTE, params=params)
     assert response.status_code == 401
     assert set(response.json().keys()) == {"detail"}
     with session_factory() as session:
-        record = session.scalar(select(WorkoutType).filter_by(id=wt_to_delete.id))
+        record = session.scalar(select(Workout).filter_by(id=wkt_to_delete.id))
         assert record.deleted_at == None
 
     # Different user auth should get a 404 because we can't find that workout type.
@@ -33,40 +31,24 @@ def test_authentication_required_for_delete(
     assert response.status_code == 404
     assert set(response.json().keys()) == {"detail"}
     with session_factory() as session:
-        record = session.scalar(select(WorkoutType).filter_by(id=wt_to_delete.id))
-        assert record.deleted_at == None
-
-
-def test_cant_delete_public_workout_type(
-    client: TestClient,
-    public_workout_type: WorkoutType,
-    session_factory: sessionmaker[Session],
-):
-    params = {"id": public_workout_type.id}
-    response = client.delete(ROUTE, params=params)
-    assert response.status_code == 401
-    assert set(response.json().keys()) == {"detail"}
-    with session_factory() as session:
-        record = session.scalar(
-            select(WorkoutType).filter_by(id=public_workout_type.id)
-        )
+        record = session.scalar(select(Workout).filter_by(id=wkt_to_delete.id))
         assert record.deleted_at == None
 
 
 def test_can_delete_own_workout_type(
     client: TestClient,
     primary_test_user: UserWithAuth,
-    primary_user_workout_types: tuple[WorkoutType, ...],
+    primary_user_workouts: tuple[Workout, ...],
     session_factory: sessionmaker[Session],
 ):
-    wt_to_delete = primary_user_workout_types[0]
-    params = {"id": wt_to_delete.id}
+    wkt_to_delete = primary_user_workouts[0]
+    params = {"id": wkt_to_delete.id}
 
     start_time = datetime.now(tz=timezone.utc)
     response = client.delete(ROUTE, params=params, headers=primary_test_user.auth)
     end_time = datetime.now(tz=timezone.utc)
     assert response.status_code == 200
     with session_factory() as session:
-        record = session.scalar(select(WorkoutType).filter_by(id=wt_to_delete.id))
+        record = session.scalar(select(Workout).filter_by(id=wkt_to_delete.id))
         assert record.deleted_at != None
         assert start_time < record.deleted_at < end_time
