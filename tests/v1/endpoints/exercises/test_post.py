ab97d7d81d6e71e4b39a8c8f566960122d893c34
Ethan Swan <ethanpswan@gmail.com>
2023-02-28 22:37:18 -0600

    Add xfailing tests for a permissions bug.

--- tests/v1/endpoints/exercises/test_post.py
+++ tests/v1/endpoints/exercises/test_post.py
@@ -1,18 +1,18 @@
 from uuid import UUID, uuid4
 
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.sql import select, delete
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi.testclient import TestClient
 
 from app.db.models.user import UserWithAuth
-from app.db import Workout
+from app.db.models import Exercise
 
 
-ROUTE = "/workouts/"
+ROUTE = "/exercises"
 
 
-def test_unauthenticated_user_cant_create_workouts(
+def test_unauthenticated_user_cant_create_exercises(
     client: TestClient, postable_payload: dict[str, str]
 ):
     # No creds
@@ -25,7 +25,7 @@ def test_unauthenticated_user_cant_create_workouts(
     assert response.status_code == 401
 
 
-def test_authenticated_user_can_create_workouts(
+def test_authenticated_user_can_create_exercises(
     client: TestClient,
     primary_test_user: UserWithAuth,
     postable_payload: dict[str, str],
@@ -42,13 +42,13 @@ def test_authenticated_user_can_create_workouts(
     # Clean up and make sure that record was in the db.
     with session_factory() as session:
         result = session.execute(
-            delete(Workout).where(Workout.id == UUID(resource["id"]))
+            delete(Exercise).where(Exercise.id == UUID(resource["id"]))
         )
         session.commit()
         assert result.rowcount == 1
 
 
-def test_invalid_workout_type_id_is_rejected(
+def test_invalid_exercise_type_id_is_rejected(
     client: TestClient,
     primary_test_user: UserWithAuth,
     postable_payload: dict[str, str],
@@ -56,31 +56,28 @@ def test_invalid_workout_type_id_is_rejected(
 ):
     payload = postable_payload.copy()
     random_id = uuid4()
-    payload["workout_type_id"] = str(random_id)
+    payload["exercise_type_id"] = str(random_id)
     response = client.post(ROUTE, json=payload, headers=primary_test_user.auth)
     assert response.status_code == 400
     with session_factory() as session:
         # Confirm that no record was added to the database.
-        query = select(Workout).where(Workout.workout_type_id == random_id)
+        query = select(Exercise).where(Exercise.exercise_type_id == random_id)
         record = session.execute(query).scalar()
         assert record is None
 
 
-def test_user_cant_create_workout_for_workout_type_that_isnt_theirs(
+def test_user_cant_create_exercise_for_workout_that_isnt_theirs(
     client: TestClient,
     secondary_test_user: UserWithAuth,
     postable_payload: dict[str, str],
-    primary_user_workouts: tuple[Workout, ...],
     session_factory: sessionmaker[Session],
 ):
-    # Get the workout_id of an existing workout, which is specific to the primary user.
-    wt_id = str(primary_user_workouts[0].workout_type_id)
     # The postable_payload references a workout_id and exercise_type_id that are both
     # specific to the primary user.
-    payload = postable_payload.copy()
-    payload["workout_type_id"] = wt_id
-    # Force an xfail in the test so that we don't actually create the resource and have
-    # to delete it.
+    # For now, force an xfail in the test so that we don't actually create the resource
+    # and have to delete it.
     pytest.xfail("This check isn't implemented in the endpoint yet.")
-    response = client.post(ROUTE, json=payload, headers=secondary_test_user.auth)
+    response = client.post(
+        ROUTE, json=postable_payload, headers=secondary_test_user.auth
+    )
     assert response.status_code == 400
