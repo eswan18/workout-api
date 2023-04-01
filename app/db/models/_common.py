ad3982779f1a763d72afdd968d296f9e1a8e8f75
Ethan Swan <eswan18@rocketmail.com>
2023-04-01 16:06:35 -0500

    Add a protocol class for better typing.

--- app/db/models/_common.py
+++ app/db/models/_common.py
@@ -1,17 +1,30 @@
 import uuid
-from typing import Iterable, cast
+from typing import Iterable, cast, Protocol
 
 from sqlalchemy.sql import Select, column, values, literal, select
+from sqlalchemy.orm import Mapped
 from sqlalchemy.types import UUID
+from sqlalchemy.sql.elements import ColumnElement
 
 from .user import User
-from app.db import Base
+
+
+class ResourceModel(Protocol):
+    """Base class for models that are resources that can be read by users."""
+
+    __tablename__: str
+    id: Mapped[uuid.UUID]
+
+    @classmethod
+    def readable_by(cls, user: User) -> ColumnElement[bool]:
+        """Build a query for IDs that are visible to this user."""
+        ...
 
 
 def missing_references_to_model_query(
     ids: Iterable[uuid.UUID],
     user: User,
-    model: type[Base],
+    model: type[ResourceModel],
 ) -> Select[tuple[uuid.UUID, str]]:
     """Build a query for IDs that aren't visible to this user, by resource."""
     ids_as_records: list[tuple[uuid.UUID, ...]] = [(id,) for id in ids]
@@ -21,7 +34,7 @@ def missing_references_to_model_query(
         # into a lot of incorrect type hints in sqlalchemy, though tests show that it
         # works fine.
         empty_query: Select[tuple[uuid.UUID, str]] = select(  # type: ignore [assignment]
-            model.id.label("ref_id"),  # type: ignore [attr-defined]
+            model.id.label("ref_id"),
             literal(model.__name__).label("ref_type"),
         ).where(
             literal(False)
@@ -31,7 +44,7 @@ def missing_references_to_model_query(
     id_values = values(column("id", UUID), name="record_ids").data(ids_as_records)
     # Find which IDs don't match up to a resource that's visible to this user.
     query: Select[tuple[uuid.UUID, str]] = (
-        select(  # type: ignore
+        select(  # type: ignore [assignment]
             column("id").label("ref_id"),
             literal(model.__name__).label("ref_type"),
         )
@@ -40,7 +53,7 @@ def missing_references_to_model_query(
             column("id").not_in(
                 # This line relies on the model class having some features that all the
                 # "standard" models do: a `id` column, and a `readable_by` method.
-                select(model.id).where(model.readable_by(user=user))  # type: ignore [attr-defined]
+                select(model.id).where(model.readable_by(user=user))
             )
         )
     )
