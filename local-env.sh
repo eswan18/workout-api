3c694b56011508d49af8e48f2910437f9666c56c
Ethan Swan <eswan18@rocketmail.com>
2023-08-27 08:49:23 -0500

    Change name of env var.

--- local-env.sh
+++ local-env.sh
@@ -1,3 +1,3 @@
-export DATABASE_URL=postgresql://test:test@localhost:6543
-export APP_SECRET=dummysecret
-export STATIC_APPLICATION_KEY=lestibournes
+export DATABASE_URL="postgresql://test:test@localhost:6543"
+export API_JWT_SECRET="dummysecret"
+export STATIC_APPLICATION_KEY="lestibournes"
